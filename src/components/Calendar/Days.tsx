d59115b9796e417db220595964f5034d96ed94ee
Alex Martin <alex@analogmemory.xyz>
2023-03-13 17:03:50 -0700

    Narrow types allowed type arg in buttonClass function

--- src/components/Calendar/Days.tsx
+++ src/components/Calendar/Days.tsx
@@ -229,7 +229,7 @@ const Days: React.FC<Props> = ({
     );
 
     const buttonClass = useCallback(
-        (day: number, type: string) => {
+        (day: number, type: "current" | "next" | "previous") => {
             const baseClass = "flex items-center justify-center w-12 h-12 lg:w-10 lg:h-10";
             return cn(
                 baseClass,
